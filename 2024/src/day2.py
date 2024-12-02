ff70b3f2618e949ebe9fdf34b5314427fb179afb
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2024-12-02 15:45:02 +0100

    Day 2 started

---
+++ 2024/src/day2.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+
+
+def main(path_=None):
+    pass
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    datapath = "DATA/"
+    filename = "day2.input"
+
+    path = os.path.join(datapath, filename)
+    main(path)
