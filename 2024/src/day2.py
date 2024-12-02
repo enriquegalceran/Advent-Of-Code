8955605793d6e458bbbaf1f506931d9275e77cb1
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2024-12-02 16:04:54 +0100

    Day 2 part 1

--- 2024/src/day2.py
+++ 2024/src/day2.py
@@ -3,7 +3,60 @@ import numpy as np
 
 
 def main(path_=None):
-    pass
+    if path_ is None:
+        data = [
+            [7, 6, 4, 2, 1],
+            [1, 2, 7, 8, 9],
+            [9, 7, 6, 2, 1],
+            [1, 3, 2, 4, 5],
+            [8, 6, 4, 4, 1],
+            [1, 3, 6, 7, 9],
+        ]
+    else:
+        with open(path_, "r") as file:
+            data = file.readlines()
+        data = [x.strip().split() for x in data]
+        data = [[int(k) for k in _] for _ in data]
+
+    print("Data:")
+    print(data)
+
+    def is_decreasing(vector):
+        for x in range(1, len(vector)):
+            if vector[x] > vector[x - 1]:
+                return False
+        return True
+
+    def is_increasing(vector):
+        for x in range(1, len(vector)):
+            if vector[x] < vector[x - 1]:
+                return False
+        return True
+
+    def within_bounds(vector, lower, upper):
+        for x in range(1, len(vector)):
+            dif = abs(vector[x] - vector[x - 1])
+            if dif > upper or dif < lower:
+                return False
+        return True
+
+    def is_safe(vector, lower=1, upper=3):
+        return (is_decreasing(vector) or is_increasing(vector)) and within_bounds(vector, lower, upper)
+
+    safe = 0
+    for i in range(len(data)):
+        vector_i = data[i]
+        decreasing = is_decreasing(vector_i)
+        increasing = is_increasing(vector_i)
+        bounds = within_bounds(vector_i, 1, 3)
+        is_safe_ = is_safe(vector_i)
+        print(f"Row {i}: Decreasing: {decreasing}, Increasing: {increasing}, Bounds: {bounds}, All: {is_safe_}")
+        safe += is_safe_
+
+    print(f"Solution Day 2, Part 1: {safe}")
+
+
+    print("here")
 
 
 
@@ -19,4 +72,4 @@ if __name__ == "__main__":
     filename = "day2.txt"
 
     path = os.path.join(datapath, filename)
-    main(path)
+    main(None)
