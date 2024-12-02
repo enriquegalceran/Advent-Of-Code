be6295b669ac8caec0f8f5f8fa064a55005d2266
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2024-12-02 16:21:03 +0100

    Day 2 part 2 Complete!

--- 2024/src/day2.py
+++ 2024/src/day2.py
@@ -1,8 +1,7 @@
 import os
-import numpy as np
 
 
-def main(path_=None):
+def main(path_=None, verbose=1):
     if path_ is None:
         data = [
             [7, 6, 4, 2, 1],
@@ -18,8 +17,9 @@ def main(path_=None):
         data = [x.strip().split() for x in data]
         data = [[int(k) for k in _] for _ in data]
 
-    print("Data:")
-    print(data)
+    if verbose > 0:
+        print("Data:")
+        print(data)
 
     def is_decreasing(vector):
         for x in range(1, len(vector)):
@@ -50,21 +50,44 @@ def main(path_=None):
         increasing = is_increasing(vector_i)
         bounds = within_bounds(vector_i, 1, 3)
         is_safe_ = is_safe(vector_i)
-        print(f"Row {i}: Decreasing: {decreasing}, Increasing: {increasing}, Bounds: {bounds}, All: {is_safe_}")
+        if verbose > 0:
+            print(f"Row {i}: Decreasing: {decreasing}, Increasing: {increasing}, Bounds: {bounds}, Safe: {is_safe_}")
         safe += is_safe_
 
     print(f"Solution Day 2, Part 1: {safe}")
 
+    # ------------------------------------------------------------------------------------------------------------------
+    # Part 2
 
-    print("here")
+    def problem_dampener(vector):
+        if is_safe(vector):
+            return True, -1
+        else:
+            for x in range(len(vector)):
+                vector2 = vector.copy()
+                vector2.pop(x)
+                if is_safe(vector2):
+                    return True, x
+            return False, -1
 
+    is_safe_dampened = 0
+    for i in range(len(data)):
+        vector_i = data[i]
+        is_safe_dampened_i, id_ = problem_dampener(vector_i)
+        if verbose > 0:
+            if id_ == -1:
+                if is_safe_dampened_i:
+                    print(f"Row {i}: {vector_i} - Is Safe!")
+                else:
+                    print(f"Row {i}: {vector_i} - Is Not Safe regardless of dampening!")
+            else:
+                dampened_vector_string = vector_i.copy()
+                dampened_vector_string[id_] = "*" + str(dampened_vector_string[id_]) + "*"
+                dampened_vector_string = str(dampened_vector_string).replace("'", "")
+                print(f"Row {i}: {dampened_vector_string} - Is Safe! Dampening at {id_}!")
+        is_safe_dampened += is_safe_dampened_i
 
-
-
-
-
-
-
+    print(f"Solution Day 2, Part 2: {is_safe_dampened}")
 
 
 if __name__ == "__main__":
@@ -72,4 +95,4 @@ if __name__ == "__main__":
     filename = "day2.txt"
 
     path = os.path.join(datapath, filename)
-    main(None)
+    main(path, verbose=0)
