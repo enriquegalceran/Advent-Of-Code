f1792065a249bbb7752c6ed615debc53b61c65e1
enriquegalceran <44613764+enriquegalceran@users.noreply.github.com>
2024-12-10 12:30:35 +0100

    Day 8 Part 2 completed!

--- 2024/src/day8.py
+++ 2024/src/day8.py
@@ -19,7 +19,6 @@ def main(path_=None, verbose=1):
             "............",
             "............",
         ]
-        # Solution = 14
     else:
         with open(path_, "r") as file:
             data = file.readlines()
@@ -29,12 +28,33 @@ def main(path_=None, verbose=1):
         print(data)
         print("\n".join(data))
 
+    coordinate_antenna_types, mapshape = parse_input_data(data)
+
+    # Part 1
+    # Test solution = 14
+    antinodes = calculate_antinodes_positions(coordinate_antenna_types, mapshape, resonant=False)
+    all_antinodes = add_antinodes_and_plot(antinodes, coordinate_antenna_types, mapshape, verbose)
+
+    print(f"Solution Day 8 - Part 1: {all_antinodes}")
+
+    # ------------------------------------------------------------------------------------------------------------------
+    # Part 2
+    # Test solution: 34
+
+    antinodes = calculate_antinodes_positions(coordinate_antenna_types, mapshape, resonant=True)
+    all_antinodes = add_antinodes_and_plot(antinodes, coordinate_antenna_types, mapshape, verbose)
+    print(f"Solution Day 8 - Part 2: {all_antinodes}")
+
+
+def parse_input_data(data):
     mapshape = (len(data), len(data[0]))
     list_antenna_types = find_all_unique_antennas(data)
     coordinate_antenna_types = get_coordinates_from_map(data, list_antenna_types, mapshape)
+    return coordinate_antenna_types, mapshape
 
+
+def calculate_antinodes_positions(coordinate_antenna_types, mapshape, resonant):
     antinodes = {}
-    total_antinodes = 0
     for antenna_type, coordinates in coordinate_antenna_types.items():
         # For each pair of antennas, find the antinode
         antinodes[antenna_type] = []
@@ -42,14 +62,17 @@ def main(path_=None, verbose=1):
             for a2 in coordinates:
                 if a1 == a2:
                     continue
-                antinode1, antinode2 = get_antinodes(a1, a2)
-                if within_map(antinode1, mapshape):
-                    antinodes[antenna_type].append(antinode1)
-                    total_antinodes += 1
-                if within_map(antinode2, mapshape):
-                    antinodes[antenna_type].append(antinode2)
-                    total_antinodes += 1
+                if resonant:
+                    antinodes_found = get_antinodes_resonant(a1, a2, mapshape)
+                else:
+                    antinodes_found = get_antinodes(a1, a2)
+                for antin in antinodes_found:
+                    if within_map(antin, mapshape):
+                        antinodes[antenna_type].append(antin)
+    return antinodes
 
+
+def add_antinodes_and_plot(antinodes, coordinate_antenna_types, mapshape, verbose):
     map_with_antinodes = np.ones(mapshape, dtype=int) * -1
     for antenna_type, coordinates in coordinate_antenna_types.items():
         i_antenna = list(coordinate_antenna_types.keys()).index(antenna_type)
@@ -58,39 +81,23 @@ def main(path_=None, verbose=1):
     for _, coordinates in antinodes.items():
         for c in coordinates:
             map_with_antinodes[c[0], c[1]] = -2
-    print_map(map_with_antinodes)
-    all_antinodes = (map_with_antinodes == -2).sum()
-
-    print(f"Solution Day 8 - Part 1: {all_antinodes}")
-
-    # ------------------------------------------------------------------------------------------------------------------
-    # Part 2
-    # Solution Test: Same input, solution now 34
+    if verbose > 0:
+        print_map(map_with_antinodes)
+    return (map_with_antinodes == -2).sum()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    print("here")
-
-
-
-
-
-
-
-
-
+def get_antinodes_resonant(x1, x2, mapshape):
+    antinodes = []
+    direction = (x2[0] - x1[0], x2[1] - x1[1])
+    antinode = x1
+    while within_map(antinode, mapshape):
+        antinodes.append(antinode)
+        antinode = (antinode[0] + direction[0], antinode[1] + direction[1])
+    antinode = (x1[0] - direction[0], x1[1] - direction[1])
+    while within_map(antinode, mapshape):
+        antinodes.append(antinode)
+        antinode = (antinode[0] - direction[0], antinode[1] - direction[1])
+    return antinodes
 
 
 def get_antinodes(x1, x2):
@@ -122,7 +129,7 @@ def print_map(map_data):
 
 def find_all_unique_antennas(data_):
     all_data = "".join(data_)
-    antennas = re.findall("[a-z]|[A-Z]|[0-9]", all_data)
+    antennas = re.findall(r"[a-z]|[A-Z]|[0-9]", all_data)
     return list(set(antennas))
 
 
